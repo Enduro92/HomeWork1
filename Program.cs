05fcd4750846d813eb8ee5a661f33c5948d7ef11
Denis <den.paranoid1992@gmail.com>
2023-02-03 15:25:38 +0300

    сделал домашку

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число :");
+string number1 = Console.ReadLine();
+int number = Convert.ToInt32(number1);
+
+if (number % 2 == 0);
+{
+    Console.WriteLine("Чётное число");
+
+}
+
+
+
